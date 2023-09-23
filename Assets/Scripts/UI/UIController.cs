8b9f6b114004497bdd0968e83a3bf0d4df208f6a
Ping3227 <b09204045@ntu.edu.tw>
2023-09-23 10:16:12 +0800

    Update UIController.cs

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -1,18 +1,83 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-/// <summary>
-/// 
-/// </summary>
-public class UIController : MonoBehaviour
-{
-    public Canvas HUDCanvas;
-    public Canvas PauseCanvas;
-    private bool IsPause;
-    // Update is called once per frame
-    void Update()
+
+
+namespace Platformer.UI {
+    /// <summary>
+    /// This class is responsible for managing the UI.
+    /// also implement the event call by UI interaction
+    /// </summary>
+    public class UIController : MonoBehaviour
     {
+        /// <summary>
+        /// The HUD canvas.
+        /// must at least contain Health,Stamina 
+        /// </summary>
+        public Canvas HUDCanvas;
+        /// <summary>
+        /// The Pause canvas.
+        /// must at least contain Resume,Quit
+        /// </summary>
+        public Canvas PauseCanvas;
+        /// <summary>
+        /// boolean to check if the game is paused
+        /// </summary>
+        private bool IsPause;
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetButtonDown("PauseMenu"))
+            {
+                IsPause = !IsPause;
+                if (IsPause)
+                {
+                    //to do, Menu Mode: sent event to pause the game
+                    if (PauseCanvas)
+                    {
+                        PauseCanvas.gameObject.SetActive(true);
+                    }
+                }
+                else {
+                    //to do, Game Mode: sent event to resume the game
+                    if (PauseCanvas)
+                    {
+                        PauseCanvas.gameObject.SetActive(false);
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Enable the HUD canvas when the game start
+        /// </summary>
+        void OnEnable()
+        {
+            if (HUDCanvas) {
+                HUDCanvas.gameObject.SetActive(true);
+            }
+            
+        }
+        /// <summary>
+        /// Disable the HUD canvas when the game end
+        /// </summary>
+        void OnDisable()
+        {
+            if (HUDCanvas)
+            {
+                HUDCanvas.gameObject.SetActive(false);
+            }
+        }
+        /// <summary>
+        /// Quit the game, bind to the Quit button
+        /// </summary>
+        void Quit() {
+            Application.Quit();
+        }
         
     }
+
+
 }
+
