718b2dbe2c3ed12aca8471bb9ccb3d51d867b563
Ping3227 <b09204045@ntu.edu.tw>
2023-09-23 09:45:10 +0800

    basic construct

---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 
+/// </summary>
+public class UIController : MonoBehaviour
+{
+    public Canvas HUDCanvas;
+    public Canvas PauseCanvas;
+    private bool IsPause;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
