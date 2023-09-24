88cc5cf2226f2914ad00bc9ac6f4d7632ef77b4a
Ping3227 <b09204045@ntu.edu.tw>
2023-09-24 17:50:27 +0800

    practice tile

---
+++ Assets/Scripts/Mechanics/Character/InventoryManager.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance { get; private set; }
+    private List<Item> items = new List<Item>();
+    private Item CurrentItem ;
+    void Awake()
+    {
+        Instance = this;
+    }
+    public void AddItem(Item item) {
+        items.Add(item);
+        if(CurrentItem == null)
+        {
+            CurrentItem = item;
+        }
+    }
+    public void RemoveItem(Item item)
+    {
+        items.Remove(item);
+        Destroy(item);
+    }
+    /// <summary>
+    /// use current item
+    /// </summary>
+    public void UseItem() {
+    }
+    public void NextItem()
+    {
+        items.
+    }
+}
