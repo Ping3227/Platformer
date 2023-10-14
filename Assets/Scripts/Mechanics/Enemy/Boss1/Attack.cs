acb89fc2b86b6c9a63ea02f1cdf01a4f4d698288
Ping3227 <b09204045@ntu.edu.tw>
2023-10-14 11:55:02 +0800

    statemachine

--- Assets/Scripts/Mechanics/Enemy/Boss1/Attack.cs
+++ Assets/Scripts/Mechanics/Enemy/Boss1/Attack.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Attack : StateMachineBehaviour { 
     GameObject player;
-    BoxCollider2D playerColl;
+    Bounds playerColl;
     Rigidbody2D rb;
     BoxCollider2D area;
     List<Vector2> options = new List<Vector2>();
@@ -14,21 +14,22 @@ public class Attack : StateMachineBehaviour {
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         player = GameController.player;
-        playerColl =player.GetComponent<BoxCollider2D>();
+        playerColl =player.GetComponent<Renderer>().bounds;
         rb = animator.GetComponent<Rigidbody2D>();
         area = animator.GetComponent<Boss>().Area;
         
-        InRange(new Vector2(playerColl.bounds.center.x,playerColl.bounds.max.y)+(attackDistance*Vector2.up));
-        InRange(new Vector2(playerColl.bounds.min.x, playerColl.bounds.center.y) + (attackDistance*Vector2.left));
-        InRange(new Vector2(playerColl.bounds.max.x, playerColl.bounds.center.y)+ (attackDistance*Vector2.right));
+        InRange(new Vector2(playerColl.center.x,playerColl.max.y)+(attackDistance*Vector2.up));
+        InRange(new Vector2(playerColl.min.x, playerColl.center.y) + (attackDistance*Vector2.left));
+        InRange(new Vector2(playerColl.max.x, playerColl.center.y)+ (attackDistance*Vector2.right));
         CurrentTime = teleportTime;
         void InRange(Vector3 position) {
             if(position.x>area.bounds.min.x && position.x<area.bounds.max.x 
                 && position.y>area.bounds.min.y && position.y<area.bounds.max.y)
                 options.Add(position);
         };
-        Debug.Log(options.Count);
+        
         targetPosition= options[Random.Range(0,options.Count)];
+        options.Clear();
     }
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
--- Assets/Scripts/Mechanics/Enemy/Boss1/Dodge.cs
+++ Assets/Scripts/Mechanics/Enemy/Boss1/Dodge.cs
@@ -7,7 +7,7 @@ public class DodgeAttack : StateMachineBehaviour
 {
     GameObject player;
     Rigidbody2D rb;
-    BoxCollider2D coll;
+    Bounds coll;
     BoxCollider2D area;
     Vector2 targetPosition;
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -15,9 +15,11 @@ public class DodgeAttack : StateMachineBehaviour
         player = GameController.player;
         rb = animator.GetComponent<Rigidbody2D>();
         area = animator.GetComponent<Boss>().Area;
-        coll = animator.GetComponent<BoxCollider2D>();
+        coll = animator.GetComponent<Renderer>().bounds;
+        Debug.Log("height: " + coll.size.y);
         if (player.transform.position.x > area.bounds.center.x) {
             targetPosition = new Vector2(area.bounds.min.x+coll.size.x, coll.size.y);
+            
         }
         else
         {
