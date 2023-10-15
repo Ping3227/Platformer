6b373897d2a6ecaa27b82c97ff7620e60de10856
Jack-0427 <A31576608@gmail.com>
2023-10-16 01:04:01 +0800

    Animation Hurt Success

---
+++ Assets/Scripts/Mechanics/Enemy/Monster/MonsterAttackCollision.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Platformer.Mechanics;
+
+public class AttackAreaCollision : MonoBehaviour
+{
+    GameObject player;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameController.player;
+        anim = player.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            anim.SetTrigger("Hurt");
+            Debug.Log("Player进入了AttackArea！");
+        }
+        else if (other.CompareTag("Enemy"))
+        {
+            Debug.Log("Enemy进入了AttackArea！");
+        }
+    }
+
+}
