290dab3b31da05a02157cea2a2c4211975f56471
Ping3227 <b09204045@ntu.edu.tw>
2023-10-18 16:35:22 +0800

    clean file

--- Assets/BT_ping/BT_Rules.cs
+++ Assets/BT_ping/BT_Rules.cs
@@ -5,47 +5,90 @@ using System.Collections.Generic;
 
 public class BT_Rules : MonoBehaviour
 {
-    [SerializeField] Animator animator;
-    bool animateSuccess =false;
+    [Header("AnimationClips")]
+    private Animation anim;
+    [SerializeField] AnimationClip attackAnimation;
+    [SerializeField] AnimationClip teleportAnimation;
+    [SerializeField] float TeleportTime ;
+    [SerializeField] AnimationClip idleAnimation;
+    [SerializeField] AnimationClip blockAnimation;
+
+    [Header("Move")]
+    private Rigidbody2D rb;
+    private Vector2 NextLocation;
     private List<Vector2> moveOptions = new List<Vector2>();
+    [SerializeField] Player player;
+    [SerializeField] BoxCollider2D Area;
+    private float DistancetoPlayer = 0f;
+
     [Task]
-    public bool IsImmobilized =false;
-    [Task]
-    public bool Far = true;
-    [Task]
-    public bool IsInfront = true;
-    [Task]
-    public bool IsBlocked = false;
-    [Task]
-    void Teleport() {
+    [HideInInspector]
+    public bool IsImmobilized = false;
+
+    private void Start()
+    {    
+        anim = gameObject.GetComponent<Animation>();
+        rb= GetComponent<Rigidbody2D>();
+
+        // Set teleport animation time
         
+        AnimationEvent ev = new AnimationEvent();
+        ev.time = TeleportTime;
+        ev.functionName = "Move";
+        teleportAnimation.AddEvent(ev);
+        
+    }
+
+    #region NonTask function
+    private void Move()
+    {
+        rb.position = NextLocation;
+    }
+    #endregion
+    #region Normal function
+    [Task]
+    void SetNextLocation(Vector2 NextLocation) {
+        if (!anim.isPlaying)
+        {
+            this.NextLocation = NextLocation;
+            ThisTask.Succeed();
+        }
+    }
+    [Task]
+    void CalculateDistance() {
+        if (!anim.isPlaying) {
+            DistancetoPlayer = Vector2.Distance(player.transform.position, transform.position);
+            ThisTask.Succeed();
+        }
+    }
+    [Task]
+    bool FartherThan(float distance) => DistancetoPlayer > distance;
+    #endregion
+    #region Animation
+    [Task]
+    void Teleport(float speed) {
+        if (!anim.isPlaying) {
+            anim[teleportAnimation.name].speed = speed;
+            anim.Play(teleportAnimation.name);
+            ThisTask.Succeed();
+        }
     }
     [Task]
     void Attack() {
-        
-    }
-    [Task]
-    void Immobilize() {
-    }
-    [Task]
-    void BackAttack()
-    {
-
-    }
-    [Task]
-    void FastTeleport() { 
-    }
-    [Task]
-    void FastAttack() { 
+        if (!anim.isPlaying)
+        {
+            anim.Play(attackAnimation.name);
+            ThisTask.Succeed();
+        }
     }
     [Task]
     void Blocking() { 
+        if (!anim.isPlaying)
+        {
+            anim.Play(blockAnimation.name);
+            ThisTask.Succeed();
+        }
     }
-    [Task]
-    void counterattack() { 
-    }
-    [Task]
-    void TeleportBack()
-    {
-    }
+    #endregion
+
 }
