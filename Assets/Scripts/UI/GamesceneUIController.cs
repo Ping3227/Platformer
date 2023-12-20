73d5a21874c1979b4873e5112bdb9f89f7f0c601
skai1122 <skai891122@gmail.com>
2023-12-20 12:17:27 +0800

    UI debug

--- Assets/Scripts/UI/GamesceneUIController.cs
+++ Assets/Scripts/UI/GamesceneUIController.cs
@@ -65,6 +65,8 @@ namespace Platformer.UI
             }
             TimeCounter = 0;
             rankButton.onClick.AddListener(LoadRankingList);
+            IsPause = false;
+            
         }
         
         void Update(){
@@ -107,6 +109,7 @@ namespace Platformer.UI
         public void LoadMenu(){
             Time.timeScale = 1f;
             PauseCanvas.gameObject.SetActive(false);
+            HUDCanvas.gameObject.SetActive(true);
             SceneManager.LoadScene("MainMenu");
         }
 
